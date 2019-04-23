38c6c8ddac7453780198b5c9c79721c109bd9e33
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-04-23 11:15:56 -0500

    changed a few things

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,11 +3,12 @@
 //$(document).ready(function() {
 
 	// INSERT JAVASCRIPT CODE SAMPLE HERE
+$("*").css("background", "#666");
 
 $("#doubles").css("color", "green");
 $("li").css("font-size", "20px");
 $("h1, p, li").css("background", "lime");
 
 $(".codeup").css("border", "1px solid red");
-
+alert($("h1").html());
 //});
