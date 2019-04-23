1dc64ee9e0cb28f9ff3adf052801dbc214fe022b
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-04-23 10:46:24 -0500

    jquery stuff

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,7 +1,13 @@
 "use strict";
 
-$(document).ready(function() {
+//$(document).ready(function() {
 
 	// INSERT JAVASCRIPT CODE SAMPLE HERE
 
-});
+$("#doubles").css("color", "green");
+$("li").css("font-size", "20px");
+$("h1, p, li").css("background", "lime");
+
+$(".codeup").css("border", "1px solid red");
+
+//});
