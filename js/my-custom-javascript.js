489679ab8e2a2a562b695f41703ec31a681648dd
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-04-23 13:52:14 -0500

    created a color vomitorioun of a page."

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,14 +1,72 @@
 "use strict";
-
+ var onOff = false;
 //$(document).ready(function() {
+function randomColor(){
+	var r = Math.floor(Math.random()*255);
+	var g = Math.floor(Math.random()*255);
+	var b = Math.floor(Math.random()*255);
+	var a = 0;
 
+	return "rgb(" + [r, g, b].join(",") + ")";
+}
+
+function wait(ms)
+{
+	var d = new Date();
+	var d2 = null;
+	do { d2 = new Date(); }
+	while(d2-d < ms);
+}
+
+
+function h1ColorFlipper(onOff){
+	let color = randomColor();
+	//console.log(color);
+	$("h1").css("background-color", color);
+	console.log(color);
+	// wait(500);
+	// 	if(onOff) {
+	//
+	// 		h1ColorFlipper(onOff);
+	// 	}
+
+}
+
+function liColorFlipper(){
+	let color = randomColor();
+	//console.log(color);
+	$("li").css("background-color", color);
+	console.log(color);
+	// wait(500);
+	// 	if(onOff) {
+	//
+	// 		h1ColorFlipper(onOff);
+	// 	}
+
+}
 	// INSERT JAVASCRIPT CODE SAMPLE HERE
-$("*").css("background", "#666");
+$('h1').click(function () {
+	h1ColorFlipper(onOff);
+});
 
-$("#doubles").css("color", "green");
-$("li").css("font-size", "20px");
-$("h1, p, li").css("background", "lime");
+$('p').dblclick(function () {
+	let color = randomColor();
+	$("p").css("background-color" , color);
+});
 
-$(".codeup").css("border", "1px solid red");
-alert($("h1").html());
+$("li").hover(function () {
+	$(this).css("background-color", randomColor())
+},
+	function () {
+	$(this).css("background-color", "white");
+} );
+
+
+$("*").hover(function () {
+		$(this).css("background-color", randomColor())
+	},
+	function () {
+		$(this).css("background-color", "white");
+	} );
+
 //});
