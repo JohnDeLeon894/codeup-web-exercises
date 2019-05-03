5a51b46cc78fd9695e004e73f8419090334071b1
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-03 15:15:58 -0500

    added map filter reduce js

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,50 @@
+//(()=>{
+"use strict";
+// Create a file named map-filter-reduce.js in your js directory and copy the users data below into it.
+const users = [
+	{
+		id: 1,
+		name: 'ryan',
+		email: 'ryan@codeup.com',
+		languages: ['clojure', 'javascript'],
+		yearsOfExperience: 5
+	},
+	{
+		id: 2,
+		name: 'luis',
+		email: 'luis@codeup.com',
+		languages: ['java', 'scala', 'php'],
+		yearsOfExperience: 6
+	},
+	{
+		id: 3,
+		name: 'zach',
+		email: 'zach@codeup.com',
+		languages: ['javascript', 'bash'],
+		yearsOfExperience: 7
+	},
+	{
+		id: 4,
+		name: 'fernando',
+		email: 'fernando@codeup.com',
+		languages: ['java', 'php', 'sql'],
+		yearsOfExperience: 8
+	},
+	{
+		id: 5,
+		name: 'justin',
+		email: 'justin@codeup.com',
+		languages: ['html', 'css', 'javascript', 'php'],
+		yearsOfExperience: 9
+	}
+];
+
+//Use .filter to create an array of user objects where each user object has at least 3
+// languages in the languages array.
+let lang3 = [];
+users.filter(n =>  (n.languages.length>==3)? lang3.push(n):"".log);
+
+
+
+
+//});
